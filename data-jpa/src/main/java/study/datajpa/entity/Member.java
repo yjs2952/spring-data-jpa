69acafa2eb2a116f9309f64d41c4591c4bd92ffe
yjs <yjs2952@naver.com>
2020-05-12 23:49:15 +0900

    인프런 spring-data-jpa 예제 도메인 모델과 동작확인

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,26 +1,47 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-@Getter
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
+
     private String username;
+
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     // 기본 생성자가 반드시 있어야 하고 private 으로 설정하면 안된다.
     // 프록시 객체 생성시 문제가 생길 수 있다.
-    protected Member() {
-    }
+//    protected Member() {
+//    }
 
     public Member(String username) {
         this.username = username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.age = age;
+        this.username = username;
+        if (team != null) {
+            changeTeam(team);
+        }
+
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
---
+++ data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -0,0 +1,52 @@
+package study.datajpa.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamA);
+        Member member4 = new Member("member4", 40, teamA);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        // 초기화
+        em.flush(); // 영속성 컨텍스트의 데이터를 강제로 DB에 insert 시켜버린다.
+        em.clear(); // DB 에 쿼리를 보내고 영속성 컨텍스트의 캐시를 지운다.
+
+        // 확인
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        for (Member m : members) {
+            System.out.println("member = " + m);
+            System.out.println("-> member.team = " + m.getTeam());
+        }
+
+    }
+}
